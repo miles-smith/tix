4c910b195829edc31b42d8214804797e576ea88e
Miles Smith <miles-smith@users.noreply.github.com>
2021-11-10 10:12:11 +0000

    Add input validation to registration endpoint

--- auth/src/routes/users/signup.ts
+++ auth/src/routes/users/signup.ts
@@ -1,9 +1,44 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
 
 const router = express.Router();
 
-router.post('/signup', (_req, res) => {
-  res.send({ message: 'TODO' });
+/**
+  TODO: Email uniqueness
+  TODO: Enumeration attack guard
+**/
+const emailValidator =
+  body('email')
+    .isEmail()
+    .withMessage('Email must be valid');
+
+/**
+  TODO: Password complexity
+  TODO: Password reuse
+**/
+const passwordMinLength = 10;
+const passwordMaxLength = 36;
+const passwordValidator =
+  body('password')
+    .trim()
+    .isLength({ min: passwordMinLength, max: passwordMaxLength })
+    .withMessage(`Password must be between ${passwordMinLength} and ${passwordMaxLength} characters`)
+
+const validationChain = [
+  emailValidator,
+  passwordValidator
+];
+
+router.post('/signup', validationChain, (req: Request, res: Response) => {
+  const validationErrors = validationResult(req);
+
+  if(!validationErrors.isEmpty()) {
+    return res.status(400).send(validationErrors.array());
+  }
+
+  // const { email, password } = req.body;
+
+  res.send({ message: 'Signed up successfully' });
 });
 
 export { router as registrationRouter };
