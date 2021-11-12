559e26b529ae4ad36b0b1763ac563069018746af
Miles Smith <miles-smith@users.noreply.github.com>
2021-11-12 16:40:12 +0000

    Add error handling middleware

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import router  from './routes';
+import { errorHandler } from './middlewares/error-handler';
 
 const app  = express();
 const port = 3000;
@@ -7,6 +8,8 @@ const port = 3000;
 app.use(express.json());
 
 app.use('/api', router);
+
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log('Listening on port', port);
