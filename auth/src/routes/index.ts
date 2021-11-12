c5ea645f95b33f3d8fbba4e8c1111d3714fd97ad
Miles Smith <miles-smith@users.noreply.github.com>
2021-11-12 16:40:45 +0000

    Add error handling for unknown routes

--- auth/src/routes/index.ts
+++ auth/src/routes/index.ts
@@ -1,8 +1,13 @@
 import express from 'express';
+import { RoutingError } from '../errors/routing-error';
 import { usersRouter } from './users';
 
 const router = express.Router();
 
 router.use('/users', usersRouter);
+
+router.all('*', () => {
+  throw new RoutingError();
+})
 
 export default router;
