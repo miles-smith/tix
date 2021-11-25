be608a9c6d06ff2f74c4286669c3f902f855ef8c
Miles Smith <miles-smith@users.noreply.github.com>
2021-11-25 12:38:52 +0000

    Add session support to auth service.
    We'll be using cookies as a transport mechanism ONLY!
    Auth will be handled by JWTs.
    
    We're doing this because it's going to be necessary for the
    server rendered approach that we're going to take.

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,13 +1,20 @@
 import express from 'express';
 import 'express-async-errors';
 import mongoose from 'mongoose';
+import cookieSession from 'cookie-session';
 import router  from './routes';
 import { errorHandler } from './middlewares/error-handler';
 
 const app  = express();
 const port = 3000;
 
+app.set('trust proxy', true);
+
 app.use(express.json());
+app.use(cookieSession({
+  signed: false,
+  secure: true,
+}));
 
 app.use('/api', router);
 
