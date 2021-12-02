18361769abc8da0880c537fc3f905a3c0e87d1c4
Miles Smith <miles-smith@users.noreply.github.com>
2021-12-02 12:48:54 +0000

    Return info about currently signed in user

--- auth/src/routes/users/current.ts
+++ auth/src/routes/users/current.ts
@@ -1,9 +1,20 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
-router.get('/current', (_req, res) => {
-  res.send({ message: 'Hello Current User' });
+router.get('/current', (req, res) => {
+  if(!req.session?.jwt) {
+    return res.send({ user: null });
+  }
+
+  try {
+    const token = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+
+    res.send({ user: token });
+  } catch (error) {
+    res.send({ user: null });
+  };
 });
 
 export { router as currentUserRouter };
