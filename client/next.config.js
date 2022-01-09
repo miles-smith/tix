ba8116a803dc2cd58fb28f50e05ae528e819d219
Miles Smith <miles-smith@users.noreply.github.com>
2022-01-09 18:14:18 +0000

    Add NextJS config file
    The config change is suggested as a means to dealing
    with NextJS not reliably picking up on file changes when
    running in a container.
    
    We'll see how things go before actually changing anything...

---
+++ client/next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  // NOTE: Uncomment this if NextJS doesn't appear to be picking up
+  // file changes; it should help.
+  // webpackDevMiddleware: (config) => {
+  //   config.watchOptions.poll = 300;
+  //
+  //   return config;
+  // }
+}
