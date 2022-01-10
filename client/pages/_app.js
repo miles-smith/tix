201f9fc62749a9c4dec01ea1ec28aeaffd16e45b
Miles Smith <miles-smith@users.noreply.github.com>
2022-01-10 18:55:58 +0000

    Inject currentUser into the props for every page.

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,7 +1,21 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import App from 'next/app';
+import { apiClient } from '../api/client';
 
 const AppComponent = ({ Component, pageProps }) => {
   return <Component {...pageProps} />
 }
+
+AppComponent.getInitialProps = async (appContext) => {
+  const appProps = await App.getInitialProps(appContext);
+  const { pageProps } = appProps
+
+  const axios = apiClient(appContext.ctx);
+  const { data } = await axios.get('/api/users/current');
+
+  return {
+    pageProps: { ...pageProps, currentUser: data.user }
+  };
+};
 
 export default AppComponent;
