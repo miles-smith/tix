142e118ab512003d59503ebe3526fda50eb6b690
Miles Smith <miles-smith@users.noreply.github.com>
2022-02-07 18:18:44 +0000

    Update auth service to pull Mongo connection string from env.

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -8,8 +8,12 @@ const start = async () => {
     throw new Error('Missing environment variable: JWT_KEY');
   }
 
+  if(!process.env.MONGO_URI) {
+    throw new Error('Missing environment variable: MONGO_URI');
+  }
+
   try {
-    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('Connected to MongoDB');
   } catch (err) {
     console.error(err);
