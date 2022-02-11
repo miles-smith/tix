5c3377f80b643bd77901222fa3301248d05c4b63
Miles Smith <miles-smith@users.noreply.github.com>
2022-02-11 14:10:33 +0000

    Remove unnecessary signup test helper.

--- packages/tickets/test/setup.ts
+++ packages/tickets/test/setup.ts
@@ -4,7 +4,6 @@ import jwt from 'jsonwebtoken';
 import { app } from '../src/app';
 
 declare global {
-  var signUp: (email?: String, passwd?: String) => Promise<void>;
   var signIn: () => string[];
 }
 
@@ -27,16 +26,6 @@ beforeEach(async () => {
 afterAll(async () => {
   await mongoose.connection.close();
 });
-
-global.signUp = async (email = 'test.user@example.com', passwd = 'my-password') => {
-  await request(app)
-    .post('/api/users/signup')
-    .send({
-      email: email,
-      password: passwd
-    })
-    .expect(201);
-}
 
 // Generates a stubbed JWT and session for the purposes of authenticating without relying on
 // external services.
