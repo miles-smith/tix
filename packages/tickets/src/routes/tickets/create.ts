2afee43baf2934ce5e07f1f0d179631d07b61d0b
Miles Smith <miles-smith@users.noreply.github.com>
2022-03-10 08:31:18 +0000

    Publish event on ticket creation.

--- packages/tickets/src/routes/tickets/create.ts
+++ packages/tickets/src/routes/tickets/create.ts
@@ -2,6 +2,8 @@ import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 import { authenticate, validateRequest } from '@elevenhotdogs-tix/common';
 import { Ticket } from '../../models/ticket';
+import { TicketCreatedPublisher } from '../../events/publishers/ticket-created-publisher';
+import { natsClient} from '../../nats-client';
 
 const router = express.Router();
 
@@ -26,6 +28,18 @@ router.post('/', authenticate, validate, async (req: Request, res: Response) => 
   const ticket = Ticket.build({ title, price, userId: req.currentUser!.id });
 
   await ticket.save();
+
+  // TODO: There is a potential point of failure here; the resource could save but the event
+  // fail to publish (e.g. perhaps the NATS connection is temporarily lost). This could lead to a
+  // data integrity issue. One possible solution is to use a db transaction to save both the
+  // resource, and the event (to a local collection), and use an out of band process (i.e. a
+  // resilient job queue/worker) to handle event publication.
+  new TicketCreatedPublisher(natsClient.stan).publish({
+    id:     ticket.id,
+    title:  ticket.get('title'),
+    price:  ticket.get('price').toString(),
+    userId: ticket.get('userId'),
+  });
 
   res
     .status(201)
