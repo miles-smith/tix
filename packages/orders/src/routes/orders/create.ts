92a157ed75ad23e4bd671d28dbbbf15dd4ae1398
Miles Smith <miles-smith@users.noreply.github.com>
2022-03-16 08:51:54 +0000

    Flesh out orders#create a little more

--- packages/orders/src/routes/orders/create.ts
+++ packages/orders/src/routes/orders/create.ts
@@ -1,8 +1,26 @@
 import express, { Request, Response } from 'express';
+import mongoose from 'mongoose';
+import { body } from 'express-validator';
+import { authenticate, validateRequest, RoutingError, NotAuthorizedError } from '@elevenhotdogs-tix/common';
 
 const router = express.Router();
 
-router.post('/', async (req: Request, res: Response) => {
+// TODO: Expecting a MongoDB document ID does introduce some slight/subtle coupling between
+// services. Might want to have a think about that, even if it's not going to be a big issue
+// in this toy project.
+const ticketValidator =
+  body('ticketId')
+    .not()
+    .isEmpty()
+    .custom((ticketId: string) => mongoose.Types.ObjectId.isValid(ticketId))
+    .withMessage('Ticket ID is invalid')
+
+const validate = [
+  ticketValidator,
+  validateRequest
+];
+
+router.post('/', authenticate, validate, async (req: Request, res: Response) => {
   res
     .status(201)
     .send({});
