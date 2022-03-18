9d5a70c74887c990ab08c4e91c4b61be30a21c0b
Miles Smith <miles-smith@users.noreply.github.com>
2022-03-18 16:22:43 +0000

    Implement order creation.

---
+++ packages/orders/src/routes/orders/__test__/create.test.ts
@@ -0,0 +1,116 @@
+import request from 'supertest';
+import mongoose from 'mongoose';
+import { app } from '../../../app';
+import { natsClient } from '../../../nats-client';
+import { Ticket } from '../../../models/ticket';
+import { Order, OrderStatus } from '../../../models/order';
+
+const createTicket = async () => {
+  const ticket = Ticket.build({
+    title: 'Test Ticket',
+    price: '1.00'
+  });
+
+  await ticket.save();
+  return ticket;
+};
+
+describe('authenticated users', () => {
+  const cookie = global.signIn();
+
+  describe('with valid inputs', () => {
+    it('creates a new order', async () => {
+      const ticket = await createTicket();
+
+      const response =
+        await request(app)
+          .post('/api/orders')
+          .set('Cookie', cookie)
+          .send({ ticketId: ticket.id });
+
+      expect(response.statusCode)
+        .toEqual(201);
+
+      expect(await Order.countDocuments())
+        .toEqual(1);
+    });
+
+    it('returns an error if ticket is reserved', async () => {
+      const ticket = await createTicket();
+      const order  = Order.build({
+        ticket,
+        userId: new mongoose.Types.ObjectId().toString(),
+        status: OrderStatus.Created,
+        expiresAt: new Date(),
+      });
+
+      await order.save();
+
+      const response =
+        await request(app)
+          .post('/api/orders')
+          .set('Cookie', cookie)
+          .send({ ticketId: ticket.id });
+
+      expect(response.statusCode)
+        .toEqual(400);
+
+      expect(await Order.countDocuments())
+        .toEqual(1);
+    });
+
+    it('publishes an event', async () => {
+      const ticket = await createTicket();
+
+      await request(app)
+        .post('/api/orders')
+        .set('Cookie', cookie)
+        .send({ ticketId: ticket.id });
+
+      expect(natsClient.stan.publish).toHaveBeenCalled();
+    });
+  });
+
+  describe('with invalid inputs', () => {
+    const ticketId = new mongoose.Types.ObjectId();
+
+    it('returns an error if ticket cannot be found', async () => {
+      const response =
+        await request(app)
+          .post('/api/orders')
+          .set('Cookie', cookie)
+          .send({ ticketId });
+
+      expect(response.statusCode)
+        .toEqual(404);
+    });
+
+    it('does not publish an event', async () => {
+      const ticketId = new mongoose.Types.ObjectId();
+
+      await request(app)
+        .post('/api/orders')
+        .set('Cookie', cookie)
+        .send({ ticketId });
+
+      expect(natsClient.stan.publish).not.toHaveBeenCalled();
+    });
+  });
+});
+
+describe('unauthenticated users', () => {
+  it('prevents unauthorized access', async () => {
+    const ticket = await createTicket();
+
+    const response =
+      await request(app)
+        .post('/api/orders')
+        .send({ ticketId: ticket.id });
+
+    expect(response.statusCode)
+      .toEqual(401);
+
+    expect(await Order.countDocuments())
+      .toEqual(0);
+  });
+});
--- packages/orders/src/routes/orders/create.ts
+++ packages/orders/src/routes/orders/create.ts
@@ -1,9 +1,15 @@
 import express, { Request, Response } from 'express';
 import mongoose from 'mongoose';
 import { body } from 'express-validator';
-import { authenticate, validateRequest, RoutingError, NotAuthorizedError } from '@elevenhotdogs-tix/common';
+import { authenticate, validateRequest } from '@elevenhotdogs-tix/common';
+import { RoutingError, BadRequestError } from '@elevenhotdogs-tix/common';
+import { Ticket } from '../../models/ticket';
+import { Order, OrderStatus } from '../../models/order';
 
 const router = express.Router();
+
+// TODO: Refactor out configuration details.
+const RESERVATION_PERIOD_SECONDS = 15 * 60; // 15 minutes.
 
 // TODO: Expecting a MongoDB document ID does introduce some slight/subtle coupling between
 // services. Might want to have a think about that, even if it's not going to be a big issue
@@ -21,9 +27,33 @@ const validate = [
 ];
 
 router.post('/', authenticate, validate, async (req: Request, res: Response) => {
+  const { ticketId } = req.body;
+  const ticket = await Ticket.findById(ticketId);
+
+  if(!ticket) {
+    // Not the best fit, given ticket id is arriving via params, and not a nested RESTful route,
+    // but meh (and we might change the routing in the future).
+    throw new RoutingError();
+  }
+
+  if(await ticket.isReserved()) {
+    throw new BadRequestError('Ticket is already reserved');
+  }
+
+  const order = Order.build({
+    ticket:    ticket,
+    userId:    req.currentUser!.id,
+    status:    OrderStatus.Created,
+    expiresAt: new Date(Date.now() + RESERVATION_PERIOD_SECONDS * 1000)
+  });
+
+  await order.save();
+
+  // TODO: Publish message to event bus
+
   res
     .status(201)
-    .send({});
+    .send(order);
 });
 
 export { router as createOrderRouter };
