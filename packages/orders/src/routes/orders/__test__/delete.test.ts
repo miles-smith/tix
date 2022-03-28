b62f113359e8b314ed36d29d930e7b557a28334a
Miles Smith <miles-smith@users.noreply.github.com>
2022-03-28 16:14:01 +0100

    Allow users to (soft) delete orders.

---
+++ packages/orders/src/routes/orders/__test__/delete.test.ts
@@ -0,0 +1,106 @@
+import request from 'supertest';
+import mongoose from 'mongoose';
+import { app } from '../../../app';
+import { TestUser } from '../../../../test/setup';
+import { Ticket, TicketDocument } from '../../../models/ticket';
+import { Order, OrderStatus } from '../../../models/order';
+
+const createTicket = async (title: string) => {
+  const ticket = Ticket.build({
+    title: title,
+    price: '1.00'
+  });
+
+  await ticket.save();
+  return ticket;
+};
+
+const createOrder = async (ticket: TicketDocument, user: TestUser) => {
+  const order = Order.build({
+    ticket: ticket,
+    userId: user.id,
+    status: OrderStatus.Complete,
+    expiresAt: new Date()
+  });
+
+  await order.save();
+  return order;
+}
+
+describe('as an authenticated user', () => {
+  const user   = global.generateTestUser();
+  const cookie = global.signIn(user);
+
+  it('cancels the order', async () => {
+    const ticket = await createTicket('Test Ticket');
+    const order  = await createOrder(ticket, user);
+    const count  = await Order.countDocuments({ status: OrderStatus.Cancelled });
+
+    order.status = OrderStatus.Created;
+    await order.save();
+
+    const response =
+      await request(app)
+        .delete(`/api/orders/${order.id}`)
+        .set('Cookie', cookie);
+
+    expect(await Order.countDocuments({ status: OrderStatus.Cancelled })).toEqual(count + 1);
+    expect(response.statusCode).toEqual(204);
+  });
+
+  it('it cannot cancel an already completed order', async () => {
+    const ticket = await createTicket('Test Ticket');
+    const order  = await createOrder(ticket, user);
+    const count  = await Order.countDocuments({ status: OrderStatus.Cancelled });
+
+    const response =
+      await request(app)
+        .delete(`/api/orders/${order.id}`)
+        .set('Cookie', cookie);
+
+    expect(response.statusCode).toBe(400);
+    expect(await Order.countDocuments({ status: OrderStatus.Cancelled })).toEqual(count);
+  });
+
+  it.todo('it dispatches an event on cancellation');
+
+  it('returns an error if the order is not found', async () => {
+    const id = new mongoose.Types.ObjectId();
+
+    const response =
+      await request(app)
+        .delete(`/api/orders/${id}`)
+        .set('Cookie', cookie);
+
+    expect(response.statusCode).toBe(404);
+  });
+
+  it('prevents unauthorized access', async () => {
+    const ticket = await createTicket('Test Ticket');
+    const order  = await createOrder(ticket, global.generateTestUser());
+
+    const response =
+      await request(app)
+        .delete(`/api/orders/${order.id}`)
+        .set('Cookie', cookie);
+
+    expect(response.statusCode).toEqual(401);
+    expect(response.body).not.toEqual(
+      expect.objectContaining({ id: order.id })
+    );
+  });
+});
+
+describe('as an unauthenticated user', () => {
+  it('prevents unauthorized access', async () => {
+    const ticket = await createTicket('Test Ticket');
+    const order  = await createOrder(ticket, global.generateTestUser());
+
+    const response =
+      await request(app)
+        .delete(`/api/orders/${order.id}`);
+
+    expect(response.statusCode)
+      .toEqual(401);
+  });
+});
--- packages/orders/src/routes/orders/delete.ts
+++ packages/orders/src/routes/orders/delete.ts
@@ -1,9 +1,32 @@
 import express, { Request, Response } from 'express';
+import { authenticate } from '@elevenhotdogs-tix/common';
+import { RoutingError, NotAuthorizedError, BadRequestError } from '@elevenhotdogs-tix/common';
+import { Order, OrderStatus } from '../../models/order';
 
 const router = express.Router();
 
-router.delete('/:id', async (req: Request, res: Response) => {
-  res.status(204);
+router.delete('/:id', authenticate, async (req: Request, res: Response) => {
+  const id    = req.params.id;
+  const order = await Order.findById(id);
+
+  if(!order) {
+    throw new RoutingError();
+  }
+
+  if(order.userId !== req.currentUser!.id) {
+    throw new NotAuthorizedError();
+  }
+
+  if(order.status === OrderStatus.Complete) {
+    throw new BadRequestError('Cannot cancel a completed order');
+  }
+
+  order.status = OrderStatus.Cancelled;
+  await order.save();
+
+  res
+    .status(204)
+    .send({});
 });
 
 export { router as deleteOrderRouter };
