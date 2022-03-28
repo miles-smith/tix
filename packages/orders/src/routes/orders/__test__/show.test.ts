d50cc1ed1ff0bf35d6b73eebbfa1400d545ac0c9
Miles Smith <miles-smith@users.noreply.github.com>
2022-03-28 14:38:09 +0100

    Complete implementation of orders#show endpoint

---
+++ packages/orders/src/routes/orders/__test__/show.test.ts
@@ -0,0 +1,90 @@
+import request from 'supertest';
+import mongoose from 'mongoose';
+import { app } from '../../../app';
+import { TestUser } from '../../../../test/setup';
+import { Ticket, TicketDocument } from '../../../models/ticket';
+import { Order, OrderStatus } from '../../../models/order';
+
+const createTicket = async (title: string) => {
+  const ticket = Ticket.build({
+    title: title,
+    price: '1.00'
+  });
+
+  await ticket.save();
+  return ticket;
+};
+
+const createOrder = async (ticket: TicketDocument, user: TestUser) => {
+  const order = Order.build({
+    ticket: ticket,
+    userId: user.id,
+    status: OrderStatus.Complete,
+    expiresAt: new Date()
+  });
+
+  await order.save();
+  return order;
+}
+
+describe('as an authenticated user', () => {
+  const userOne = global.generateTestUser();
+  const userTwo = global.generateTestUser();
+
+  const cookie  = global.signIn(userOne);
+
+  it('returns the order', async () => {
+    const ticket = await createTicket('Test Ticket');
+    const order  = await createOrder(ticket, userOne);
+
+    const response =
+      await request(app)
+        .get(`/api/orders/${order.id}`)
+        .set('Cookie', cookie);
+
+    expect(response.statusCode).toEqual(200);
+    expect(response.body).toEqual(
+      expect.objectContaining({ id: order.id })
+    );
+  });
+
+  it('returns an error if the order is not found', async () => {
+    const id = new mongoose.Types.ObjectId();
+
+    const response =
+      await request(app)
+        .get(`/api/orders/${id}`)
+        .set('Cookie', cookie);
+
+    expect(response.statusCode).toBe(404);
+  });
+
+  it('prevents unauthorized access', async () => {
+    const ticket = await createTicket('Test Ticket');
+    const order  = await createOrder(ticket, userTwo);
+
+    const response =
+      await request(app)
+        .get(`/api/orders/${order.id}`)
+        .set('Cookie', cookie);
+
+    expect(response.statusCode).toEqual(401);
+    expect(response.body).not.toEqual(
+      expect.objectContaining({ id: order.id })
+    );
+  });
+});
+
+describe('as an unauthenticated user', () => {
+  it('prevents unauthorized access', async () => {
+    const ticket = await createTicket('Test Ticket');
+    const order  = await createOrder(ticket, global.generateTestUser());
+
+    const response =
+      await request(app)
+        .get(`/api/orders/${order.id}`);
+
+    expect(response.statusCode)
+      .toEqual(401);
+  });
+});
--- packages/orders/src/routes/orders/show.ts
+++ packages/orders/src/routes/orders/show.ts
@@ -1,11 +1,24 @@
 import express, { Request, Response } from 'express';
+import { authenticate, RoutingError, NotAuthorizedError } from '@elevenhotdogs-tix/common';
+import { Order } from '../../models/order';
 
 const router = express.Router();
 
-router.get('/:id', async (req: Request, res: Response) => {
+router.get('/:id', authenticate, async (req: Request, res: Response) => {
+  const id = req.params.id;
+  const order = await Order.findById(id).populate('ticket');
+
+  if(!order) {
+    throw new RoutingError();
+  }
+
+  if(order.userId !== req.currentUser!.id) {
+    throw new NotAuthorizedError();
+  }
+
   res
     .status(200)
-    .send({});
+    .send(order);
 });
 
 export { router as showOrderRouter };
