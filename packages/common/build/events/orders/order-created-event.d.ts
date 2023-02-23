d8d52950d4f759494fea09f3141e6699232f5951
Miles Smith <miles-smith@users.noreply.github.com>
2023-02-23 14:30:20 +0000

    Patch and rerelease common module.
    Fixes issue with wrong versioning on order events.

--- packages/common/build/events/orders/order-created-event.d.ts
+++ packages/common/build/events/orders/order-created-event.d.ts
@@ -4,12 +4,12 @@ export interface OrderCreatedEvent {
     subject: Subjects.OrderCreated;
     data: {
         id: string;
+        version: number;
         status: OrderStatus;
         userId: string;
         expiresAt: string;
         ticket: {
             id: string;
-            version: number;
             price: string;
         };
     };
