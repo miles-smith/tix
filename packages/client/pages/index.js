7c390fe7cf104a96d7f4fc114746304ef8db2163
Miles Smith <miles-smith@users.noreply.github.com>
2023-03-07 16:51:26 +0000

    Repurpose home page as ticket listing page.

--- packages/client/pages/index.js
+++ packages/client/pages/index.js
@@ -1,16 +1,45 @@
-const HomePage = ({ currentUser }) => {
+import currency from "currency.js";
+import { apiClient } from "../api/client";
+
+const HomePage = ({ currentUser, tickets }) => {
+  const gbp = (value) => currency(value.toString(), { symbol: '£' });
+
   return(
-    <section>
+    <>
       <header>
-        <h1>Hello NextJS</h1>
+        <h1>Tickets</h1>
       </header>
-      <footer>
-        <small>
-          {currentUser && `Signed in as ${currentUser.email}`}
-        </small>
-      </footer>
-    </section>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            tickets.map(ticket => {
+              return(
+                <tr key={ticket.id} className="ticket" id={`ticket-${ticket.id}`}>
+                  <td>{ticket.title}</td>
+                  <td>{gbp(ticket.price).format()}</td>
+                </tr>
+              );
+            })
+          }
+        </tbody>
+      </table>
+    </>
   );
 }
+
+HomePage.getInitialProps = async (context) => {
+  const axios = apiClient(context);
+  const { data } = await axios.get('/api/tickets');
+
+  return {
+    tickets: data
+  }
+};
 
 export default HomePage;
